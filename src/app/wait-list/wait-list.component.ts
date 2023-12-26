27909293c0c20f3be79db07e9330ef6d56221540
cesar Santillana <icetom6@hotmail.com>
2023-12-25 23:44:40 -0500

    agregando logica, diseño tablas

--- src/app/wait-list/wait-list.component.ts
+++ src/app/wait-list/wait-list.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { NgFor } from '@angular/common'; 
 import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-wait-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './wait-list.component.html',
   styleUrl: './wait-list.component.css'
 })
+
+@Injectable({
+  providedIn: 'root',
+})
+
+
 export class WaitListComponent {
+  datos: any;
+  constructor(private http: HttpClient,  private location: Location) {}
 
-  constructor(private location: Location) {}
+  getData(): Observable<any> {
+    const url = 'http://localhost:3000/wait/all';
+    return this.http.get<any>(url);
+  }
 
+  ngOnInit(): void {
+      this.getData().subscribe(
+        (response) => {
+          this.datos = response;
+        }, 
+        (error) => {
+          console.log("Error al traer los datos", error)
+        }
+      )
+  }
   regresar(): void {
     this.location.back();
   }
